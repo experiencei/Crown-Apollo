1231290f4666910d31b4de3e829ffe75033653cc
experiencei <ayelojahighbee01@gmail.com>
2021-10-02 10:13:09 +0100

    using apollo to make query from graphql

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache}  from "apollo-cache-inmemory";
+import { ApolloClient }  from "apollo-boost";
+ 
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
+
+const httpLink = createHttpLink({
+  uri: "https:crwn-clothing.com"
+})
+const
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
